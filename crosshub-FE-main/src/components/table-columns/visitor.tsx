5c899623037288f7e703ff87288309006909b7b4
imdla <imdla918@gmail.com>
2025-06-13 14:52:18 +0900

    [modfy] manager 방문객리스트 스타일 수정

--- crosshub-FE-main/src/components/table-columns/visitor.tsx
+++ crosshub-FE-main/src/components/table-columns/visitor.tsx
@@ -6,32 +6,44 @@ const columns: ColumnDef<SiteVisitor>[] = [
     id: "userNum",
     accessorKey: "id",
     header: () => <div className="text-center">No</div>,
-    cell: (row) => <div className="text-center">{row.getValue<string>()}</div>,
+    cell: (row) => (
+      <div className="text-center text-[#333333]">{row.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "user.name",
     header: () => <div className="text-center">이름</div>,
-    cell: (row) => <div className="text-center">{row.getValue<string>()}</div>,
+    cell: (row) => (
+      <div className="text-center text-[#333333]">{row.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "createdAt",
     header: () => <div className="text-center">방문일시</div>,
-    cell: (row) => <div className="text-center">{row.getValue<string>()}</div>,
+    cell: (row) => (
+      <div className="text-center text-[#333333]">{row.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "user.countryCode",
     header: () => <div className="text-center">국가</div>,
-    cell: (row) => <div className="text-center">{row.getValue<string>()}</div>,
+    cell: (row) => (
+      <div className="text-center text-[#333333]">{row.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "user.cityId",
     header: () => <div className="text-center">명예시민</div>,
-    cell: (row) => <div className="text-center">{row.getValue<string>()}</div>,
+    cell: (row) => (
+      <div className="text-center text-[#333333]">{row.getValue<string>()}</div>
+    ),
   },
   {
     accessorKey: "user.birthday",
     header: () => <div className="text-center">생년월일</div>,
-    cell: (row) => <div className="text-center">{row.getValue<string>()}</div>,
+    cell: (row) => (
+      <div className="text-center text-[#333333]">{row.getValue<string>()}</div>
+    ),
   },
 ];
 
