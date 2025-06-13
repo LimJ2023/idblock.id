a30dfdb5a6b67dec469fa83f770adefa05b85f3a
imdla <imdla918@gmail.com>
2025-06-13 14:17:12 +0900

    [modify] manager qr촬영 페이지 스타일 수정

--- crosshub-FE-main/src/pages/manager/qr.tsx
+++ crosshub-FE-main/src/pages/manager/qr.tsx
@@ -5,18 +5,25 @@ const QrPage = () => {
   //   const { data: result } = useSuspenseQuery({ ...queries.faq.all });
 
   return (
-    <main className="flex h-full w-full flex-col bg-neutral-100 p-8">
-      <header className="mb-10 flex w-full flex-col gap-2 py-6">
-        <div className="flex justify-between px-4">
-          <h1 className="text-4xl font-semibold">QR 촬영</h1>
-        </div>
+    <main className="flex h-dvh w-full flex-col border bg-white text-[#1E1E1E]">
+      <header className="p-5">
+        <h1 className="px-4 py-2 text-3xl font-semibold text-[#1E1E1E]">
+          QR 촬영
+        </h1>
       </header>
-      <section className="flex w-full flex-1 flex-col items-center justify-center gap-6 rounded-3xl p-6">
-        <Camera size={120} strokeWidth={1} />
-        <div className="mb-10 text-xl">
-          QR코드를 촬영하시려면 아래 버튼을 눌러주세요.
+
+      <section className="h-full w-full items-center justify-center border-t border-[#E5E7EB] bg-[#FAFBFC] p-6">
+        <div className="flex h-full w-full items-center justify-center rounded-3xl border border-[#E5E7EB] bg-white px-10 py-10">
+          <div className="flex w-[20rem] flex-col items-center justify-center gap-6">
+            <div className="flex h-[127px] w-[127px] items-center justify-center rounded-full border border-[#E5E7EB] bg-[#F7F7F7]">
+              <Camera size={56} strokeWidth={1} color="#555555" />
+            </div>
+            <div className="text-base text-[#666666]">
+              QR코드를 촬영해 방문객을 승인할 수 있습니다.
+            </div>
+            <QrScanDialog />
+          </div>
         </div>
-        <QrScanDialog />
       </section>
     </main>
   );
